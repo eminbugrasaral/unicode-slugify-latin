eab8f6f81333a04c24694c6c5f6769a2cf60ca8f
Emin Bugra <eminbugrasaral@me.com>
2015-01-10 09:37:45 -0800

    Enable replacement of common Turkish latin characters with similar ascii letters.

--- slugify/__init__.py
+++ slugify/__init__.py
@@ -19,16 +19,32 @@ def smart_text(s, encoding='utf-8', errors='strict'):
             s = six.text_type(bytes(s), encoding, errors)
     else:
         s = six.text_type(s)
+
     return s
 
 
 # Extra characters outside of alphanumerics that we'll allow.
 SLUG_OK = '-_~'
 
+TURKISH_LETTERS = {
+    u'\u0131': 'i',
+    u'\u0130': 'I',
+    u'\u015f': 's',
+    u'\u015e': 'S',
+    u'\xe7': 'c',
+    u'\xc7': 'C',
+    u'\u011f': 'g',
+    u'\u011e': 'G',
+    u'\xf6': 'o',
+    u'\xd6': 'O',
+    u'\xfc': 'u',
+    u'\xdc': 'U'
+}
 
-def slugify(s, ok=SLUG_OK, lower=True, spaces=False):
+def slugify(s, ok=SLUG_OK, lower=True, spaces=False, replace_turkish=False):
     # L and N signify letter/number.
     # http://www.unicode.org/reports/tr44/tr44-4.html#GC_Values_Table
+
     rv = []
     for c in unicodedata.normalize('NFKC', smart_text(s)):
         cat = unicodedata.category(c)[0]
@@ -39,4 +55,11 @@ def slugify(s, ok=SLUG_OK, lower=True, spaces=False):
     new = ''.join(rv).strip()
     if not spaces:
         new = re.sub('[-\s]+', '-', new)
-    return new.lower() if lower else new
+    result = new.lower() if lower else new
+
+    # Turkish hack
+    if replace_turkish:
+        for letter in TURKISH_LETTERS:
+            result = result.replace(letter, TURKISH_LETTERS[letter])
+
+    return result
