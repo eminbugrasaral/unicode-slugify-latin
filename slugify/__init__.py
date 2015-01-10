6ce837bc0ab506d0ca45790a973b92c6558862f7
Emin Bugra <eminbugrasaral@me.com>
2015-01-10 11:35:32 -0800

    Update application logic.

--- slugify/__init__.py
+++ slugify/__init__.py
@@ -30,6 +30,15 @@ SLUG_REPLACE = ':;/'
 
 TURKISH_LETTERS = {
     u'\u0131': 'i',
+    u'\u015f': 's',
+    u'\xe7': 'c',
+    u'\u011f': 'g',
+    u'\xf6': 'o',
+    u'\xfc': 'u'
+}
+
+ALL_TURKISH_LETTERS = {
+    u'\u0131': 'i',
     u'\u0130': 'I',
     u'\u015f': 's',
     u'\u015e': 'S',
@@ -63,7 +72,11 @@ def slugify(s, ok=SLUG_OK, lower=True, spaces=False, replace_turkish=False):
 
     # Turkish hack
     if replace_turkish:
-        for letter in TURKISH_LETTERS:
-            result = result.replace(letter, TURKISH_LETTERS[letter])
+        if lower:
+            for letter in TURKISH_LETTERS:
+                result = result.replace(letter, TURKISH_LETTERS[letter])
+        else:
+            for letter in ALL_TURKISH_LETTERS:
+                result = result.replace(letter, TURKISH_LETTERS[letter])
 
     return result
